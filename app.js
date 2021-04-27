d876cf0c1046fe1fcd37ede8136d8ead4401f725
Happy Bhesdadiya <happy.bhesdadiya@bacancy.com>
2021-04-27 15:04:07 +0530

    Node Mailer Added

---
+++ app.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+const nodemailer = require('nodemailer');
+
+// Sending Email with Gmail
+let transport = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD
+  }
+});
+
+// Sending a Text Email
+// const mailOptions = {
+//   from: 'happypatel95370@bacancy.com', // Sender address
+//   to: 'happy.bhesdadiya@bacancy.com',  // List of recipients
+//   subject: 'Node Mailer', // Subject line
+//   text: 'Hello People!, Welcome to Bacancy!', // Plain text body
+// };
+
+// Sending an HTML Email with Attachment
+const mailOptions = {
+  from: 'happypatel95370@bacancy.com', // Sender address
+  to: 'happy.bhesdadiya@bacancy.com',  // List of recipients
+  subject: 'Node Mailer', // Subject line
+  html: '<h1 style="color:#ff6600;">Hello People!, Welcome to Bacancy!</h1>',
+  attachments: [
+    { filename: 'profile.png', path: './images/profile.png' }
+  ]
+};
+
+transport.sendMail(mailOptions, function(err, info) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log(info);
+  }
+});
